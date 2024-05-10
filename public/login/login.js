77376f6b93811344e2b513c2b4018beaed269dd1
[AI10] 김해강 <dlsel137@naver.com>
2024-05-10 23:25:39 +0900

    /

--- public/login/login.js
+++ public/login/login.js
@@ -1,13 +1,13 @@
 // 버튼 클릭 이벤트 설정
-document.getElementById('login').addEventListener('click', onSubmitButton);
-document.getElementById('register').addEventListener('click', movePage);
+document.getElementById("login").addEventListener("click", onSubmitButton);
+document.getElementById("register").addEventListener("click", movePage);
 
-document.getElementsByName('username')[0].addEventListener('keyup', (event) => {
+document.getElementsByName("username")[0].addEventListener("keyup", (event) => {
     if (event.key === "Enter") {
         onSubmitButton();
     }
 });
-document.getElementsByName('password')[0].addEventListener('keyup', (event) => {
+document.getElementsByName("password")[0].addEventListener("keyup", (event) => {
     if (event.key === "Enter") {
         onSubmitButton();
     }
@@ -15,19 +15,19 @@ document.getElementsByName('password')[0].addEventListener('keyup', (event) => {
 
 // 로그인
 async function onSubmitButton() {
-    const email = document.getElementsByName("username")[0].value.trim();
+    const email = document.getElementsByName("email")[0].value.trim();
     const password = document.getElementsByName("password")[0].value.trim();
     // 유효성 체크
     if (!checkText({ email, password })) return;
     // 데이터 불러오기 및 에러처리
-    const { response } = await sendRequest('/api/login', {
+    const { response } = await sendRequest("/api/login", {
         Method: "POST",
         ContentType: "application/json",
-        BodyData: JSON.stringify({ email, password })
+        BodyData: JSON.stringify({ email, password }),
     });
     // 성공적으로 불러왔다면 '/'경로로 이동
     if (response.ok) {
-        location.href = '../listpage/listpage.html';
+        location.href = "../listpage/listpage.html";
     }
 }
 
