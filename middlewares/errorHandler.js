ac0541933c6c09fa17d4bdf575389677f96aa044
[AI10] 김해강 <dlsel137@naver.com>
2024-05-11 01:17:05 +0900

    errorHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,15 +2,13 @@ const errorHandler = (error, req, res, next) => {
     const { name, message, statusCode } = error;
     if (statusCode >= 500 || statusCode === undefined) {
         console.error(name, message);
-        res.statusCode = 500;
-        res.json({
+        res.status(500).json({
             error: "서버에서 에러가 발생하였습니다. 자세한 내용은 개발자에게 문의해주세요.",
             data: null,
         });
         return;
     }
-    res.statusCode = statusCode;
-    res.json({ error: message, data: null });
+    res.status(statusCode).json({ error: message, data: null });
 };
 
 module.exports = errorHandler;
